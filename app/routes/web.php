b445ea0de498bbe8f91010d41b68c15754b0225f
Michael Ozoudeh <jesmikky@gmail.com>
2022-07-06 22:59:38 +0100

    update

--- app/routes/web.php
+++ app/routes/web.php
@@ -20,8 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-
 
 require __DIR__.'/auth.php';
